a84c58df70221216ab303f1fa23aa07aa5a9bdf9
Andrew Rafael Oliveira Queiroz <20261041110027@ifrn.local>
2026-08-12 10:21:01 -0300

    feat: adding the menu of the system

---
+++ Sistema de Controle de Toalhas em uma Escola de Natação/sistema.py
@@ -0,0 +1,25 @@
+codigo = []
+nome = []
+quantidade = []
+
+while True:
+    print("=================================")
+    print(" NADO LIVRE")
+    print("=================================")
+    print(" ")
+    print("1 - Cadastrar nadador")
+    print("2 - Consultar nadadores")
+    print("3 - Registrar retirada de toalhas")
+    print("4 - Consultar toalhas em uso")
+    print("0 - Sair")
+    print("")
+    opçao = input("Escolha uma opção:")
+
+
+    if opçao == 1:
+        print("===== CADASTRO DE NADADOR =====")
+        print("")
+        codigo = input("Código:")
+        nome = input("Nome:")
+
+        if codigo.isdigit() and nome.isalnum():
