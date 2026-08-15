850445836b60acabccdf9fe5381880203f6154c0
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-08-15 17:06:18 -0300

    feat: Solving problems in python

--- Problemas python/Problemas beecrowd.py/bee1021again.py
+++ Problemas python/Problemas beecrowd.py/bee1021again.py
@@ -11,14 +11,14 @@ for moeda in moedas:
     divisivel = number//moeda
     number = number- moeda*divisivel
     quantidade.append(divisivel)
-print("NOTAS: ")
+print("NOTAS:")
 print(f"{quantidade[0]} nota(s) de R$ 100.00")
 print(f"{quantidade[1]} nota(s) de R$ 50.00")
 print(f"{quantidade[2]} nota(s) de R$ 20.00")
 print(f"{quantidade[3]} nota(s) de R$ 10.00")
 print(f"{quantidade[4]} nota(s) de R$ 5.00")
 print(f"{quantidade[5]} nota(s) de R$ 2.00")
-print("MOEDAS: ")
+print("MOEDAS:")
 print(f"{quantidade[6]} moeda(s) de R$ 1.00")
 print(f"{quantidade[7]} moeda(s) de R$ 0.50")
 print(f"{quantidade[8]} moeda(s) de R$ 0.25")
