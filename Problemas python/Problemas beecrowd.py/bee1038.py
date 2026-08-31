96a97cb5863bfb2de5d2d3e031f5c3cfeb050b56
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-08-31 17:59:37 -0300

    feat: solving problems

--- Problemas python/Problemas beecrowd.py/bee1038.py
+++ Problemas python/Problemas beecrowd.py/bee1038.py
@@ -1,4 +1,4 @@
-pedido_usuario = map(input().split())
-possiveis_pedidos = {"1": "R$4.00","2": "R$4.50", "3": "R$5.00", "4": "R$2.00", "5": "R$1.50"}
-total = possiveis_pedidos.get(pedido_usuario)
-print(f"TOTAL:R${total}")
+pedido_usuario,quantidade = map(int,input().split())
+possiveis_pedidos = {1: 4.00,2: 4.50, 3: 5.00, 4: 2.00, 5: 1.50}
+total = possiveis_pedidos.get(pedido_usuario)*quantidade
+print(f"TOTAL:R$ {total:.2f}")
---
+++ Problemas python/Python Scripts/working_strings.py
@@ -0,0 +1,16 @@
+# nome = "Curso em vídeo Python"
+# print(nome[9::2])  
+# contar_v = nome.count("v", 3,14)
+# nome.replace("Maria", "Tutu")
+# print(contar_v)
+# print(nome)
+# frase = "Tutu  ama  ama programar"
+# print(frase.find("ama"))
+# Exercício 1
+# nome_completo = input("Digite seu nome completo: ")
+# print(nome_completo.upper())
+# print(nome_completo.lower())
+# print(len(nome_completo.strip().replace(" ","")))
+# print(nome_completo.split()[0])
+# Exercício 2
+numero = int(input("Digite um numero entre 1 e 9999: "))
