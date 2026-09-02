a435323033e10ed93252a3782517f4e3ae903057
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-09-02 19:07:22 -0300

    feat: solving problems

--- Lógica de programação/Listas,  tuplas e Arrays/cartas_UNO.py
+++ Lógica de programação/Listas,  tuplas e Arrays/cartas_UNO.py
@@ -1,3 +1,10 @@
+quantas_cartas = []
+print("===== INVENTÁRIO DE CARTAS =====")
 while True:
     identificacao_carta = int(input("Digite a identificação da carta (0 para encerrar): "))
-    
+    if identificacao_carta == 0:
+        break
+    quantas_cartas.append(identificacao_carta)
+    quantas_cartas_set = set(quantas_cartas)
+    for i in range(quantas_cartas_set):
+        
