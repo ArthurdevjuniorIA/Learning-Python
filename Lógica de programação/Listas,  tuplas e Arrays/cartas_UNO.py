7dac1a007f123554b1e459c46fa2212def72acaf
Arthur Kauã <kaua.arthur@escolar.ifrn.edu.br>
2026-09-03 19:05:06 -0300

    Solving this problems finally

--- Lógica de programação/Listas,  tuplas e Arrays/cartas_UNO.py
+++ Lógica de programação/Listas,  tuplas e Arrays/cartas_UNO.py
@@ -1,10 +1,17 @@
-quantas_cartas = []
+quantas_cartas_sem_repeticao = []
+quantas_cartas_total = [ ]
 print("===== INVENTÁRIO DE CARTAS =====")
 while True:
     identificacao_carta = int(input("Digite a identificação da carta (0 para encerrar): "))
     if identificacao_carta == 0:
+        print(" ===== RELATÓRIO DO INVENTÁRIO =====")
+        for i in range(len(quantas_cartas_sem_repeticao)):
+                for n in range(len(quantas_cartas_total)):
+                    conta = quantas_cartas_total.count(quantas_cartas_sem_repeticao[i])
+                print(f"Carta {quantas_cartas_sem_repeticao[i]}: {conta}")
         break
-    quantas_cartas.append(identificacao_carta)
-    quantas_cartas_set = set(quantas_cartas)
-    for i in range(quantas_cartas_set):
-        
+    if identificacao_carta not in quantas_cartas_total:
+        quantas_cartas_sem_repeticao.append(identificacao_carta)
+    quantas_cartas_total.append(identificacao_carta)
+    
+    
